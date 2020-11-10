aabf56ac2d8c5c130c5d06b462855a82d82f0903
José M. Viñas <josemanuelvinhascid@gmail.com>
2020-11-10 13:07:13 +0100

    Excepción modificada

--- src/es/uvigo/esei/dai/hybridserver/HybridServer.java
+++ src/es/uvigo/esei/dai/hybridserver/HybridServer.java
@@ -123,7 +123,7 @@ public class HybridServer {
 		try {
 			threadPool.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS);
 		} catch (InterruptedException e) {
-			throw new RuntimeException(e);
+			throw new RuntimeException(e)
 		}
 	}
 }
